54ed18fb25a17f930872bae62244df54de347f38
SupineCup <114007597+SupineCup@users.noreply.github.com>
2022-10-08 19:07:52 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+int main() {
+	//1
+	/*int hp;
+	cin >> hp;
+	bool tr = (hp > 0);
+	bool e = (hp < 0);
+	cout << e << endl;*/
+
+
+	//2 
+	/*int on, off, b;
+	cin >> b;
+	if (b > 20)
+		cout << on << endl;
+	cout << "off" << endl;*/
+
+//3
+/*int d, f;
+int tp;
+cin >> d >> tp >> f;
+if (d << 80 && tp << f)
+{
+	cout << on << endl;
+}
+else
+	cout << "off" << endl;*/
+
+	//4
+	/*double x, y;
+	cin >> x >> y;
+	if (x > 0) {
+		y = sqrt(x - 2);
+	}
+	else if (x == 0) {
+		y = x;
+	}
+	else if (x < 0) {
+		y = abs(x);
+	}
+	cout << y << endl;*/
+	
+	//5
+	/*int A;
+	cin >> A;
+	int B;
+	cin >> B;
+	switch (A) {
+	case 1: cout << "bolsho palec" << endl; break;
+	case 2: cout << "ykasateln palec" << endl; break;
+	case 3: cout << "credni palec" << endl; break;
+	case 4: cout << "bezamanu palec" << endl; break;
+	case 5: cout << "misinec" << endl; break;
+	default:cout << "oshibka" << endl;*/
+	}
